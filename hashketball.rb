95c93e47ff7b22c4153a8257c63b6a4997a41bf5
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-06-07 00:43:07 -0600

    commit tree 286ca7272b7651abc6846ba68f967141a61a9b0f

--- hashketball.rb
+++ hashketball.rb
@@ -1,129 +1,188 @@
 # Write your code below game_hash
 def game_hash
   {
-    home: {
-      team_name: "Brooklyn Nets",
-      colors: ["Black", "White"],
-      players: [
-        {
-          player_name: "Alan Anderson",
-          number: 0,
-          shoe: 16,
-          points: 22,
-          rebounds: 12,
-          assists: 12,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 1
-        },
-        {
-          player_name: "Reggie Evans",
-          number: 30,
-          shoe: 14,
-          points: 12,
-          rebounds: 12,
-          assists: 12,
-          steals: 12,
-          blocks: 12,
-          slam_dunks: 7
-        },
-        {
-          player_name: "Brook Lopez",
-          number: 11,
-          shoe: 17,
-          points: 17,
-          rebounds: 19,
-          assists: 10,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 15
-        },
-        {
-          player_name: "Mason Plumlee",
-          number: 1,
-          shoe: 19,
-          points: 26,
-          rebounds: 11,
-          assists: 6,
-          steals: 3,
-          blocks: 8,
-          slam_dunks: 5
-        },
-        {
-          player_name: "Jason Terry",
-          number: 31,
-          shoe: 15,
-          points: 19,
-          rebounds: 2,
-          assists: 2,
-          steals: 4,
-          blocks: 11,
-          slam_dunks: 1
-        }
-      ]
-    },
-    away: {
-      team_name: "Charlotte Hornets",
-      colors: ["Turquoise", "Purple"],
-      players: [
-        {
-          player_name: "Jeff Adrien",
-          number: 4,
-          shoe: 18,
-          points: 10,
-          rebounds: 1,
-          assists: 1,
-          steals: 2,
-          blocks: 7,
-          slam_dunks: 2
-        },
-        {
-          player_name: "Bismack Biyombo",
-          number: 0,
-          shoe: 16,
-          points: 12,
-          rebounds: 4,
-          assists: 7,
-          steals: 22,
-          blocks: 15,
-          slam_dunks: 10
-        },
-        {
-          player_name: "DeSagna Diop",
-          number: 2,
-          shoe: 14,
-          points: 24,
-          rebounds: 12,
-          assists: 12,
-          steals: 4,
-          blocks: 5,
-          slam_dunks: 5
-        },
-        {
-          player_name: "Ben Gordon",
-          number: 8,
-          shoe: 15,
-          points: 33,
-          rebounds: 3,
-          assists: 2,
-          steals: 1,
-          blocks: 1,
-          slam_dunks: 0
-        },
-        {
-          player_name: "Kemba Walker",
-          number: 33,
-          shoe: 15,
-          points: 6,
-          rebounds: 12,
-          assists: 12,
-          steals: 7,
-          blocks: 5,
-          slam_dunks: 12
-        }
-      ]
-    }
+	home: {
+	  team_name: "Brooklyn Nets",
+	  colors: ["Black", "White"],
+	  players: [
+		{
+		  player_name: "Alan Anderson",
+		  number: 0,
+		  shoe: 16,
+		  points: 22,
+		  rebounds: 12,
+		  assists: 12,
+		  steals: 3,
+		  blocks: 1,
+		  slam_dunks: 1
+		},
+		{
+		  player_name: "Reggie Evans",
+		  number: 30,
+		  shoe: 14,
+		  points: 12,
+		  rebounds: 12,
+		  assists: 12,
+		  steals: 12,
+		  blocks: 12,
+		  slam_dunks: 7
+		},
+		{
+		  player_name: "Brook Lopez",
+		  number: 11,
+		  shoe: 17,
+		  points: 17,
+		  rebounds: 19,
+		  assists: 10,
+		  steals: 3,
+		  blocks: 1,
+		  slam_dunks: 15
+		},
+		{
+		  player_name: "Mason Plumlee",
+		  number: 1,
+		  shoe: 19,
+		  points: 26,
+		  rebounds: 11,
+		  assists: 6,
+		  steals: 3,
+		  blocks: 8,
+		  slam_dunks: 5
+		},
+		{
+		  player_name: "Jason Terry",
+		  number: 31,
+		  shoe: 15,
+		  points: 19,
+		  rebounds: 2,
+		  assists: 2,
+		  steals: 4,
+		  blocks: 11,
+		  slam_dunks: 1
+		}
+	  ]
+	},
+	away: {
+	  team_name: "Charlotte Hornets",
+	  colors: ["Turquoise", "Purple"],
+	  players: [
+		{
+		  player_name: "Jeff Adrien",
+		  number: 4,
+		  shoe: 18,
+		  points: 10,
+		  rebounds: 1,
+		  assists: 1,
+		  steals: 2,
+		  blocks: 7,
+		  slam_dunks: 2
+		},
+		{
+		  player_name: "Bismack Biyombo",
+		  number: 0,
+		  shoe: 16,
+		  points: 12,
+		  rebounds: 4,
+		  assists: 7,
+		  steals: 22,
+		  blocks: 15,
+		  slam_dunks: 10
+		},
+		{
+		  player_name: "DeSagna Diop",
+		  number: 2,
+		  shoe: 14,
+		  points: 24,
+		  rebounds: 12,
+		  assists: 12,
+		  steals: 4,
+		  blocks: 5,
+		  slam_dunks: 5
+		},
+		{
+		  player_name: "Ben Gordon",
+		  number: 8,
+		  shoe: 15,
+		  points: 33,
+		  rebounds: 3,
+		  assists: 2,
+		  steals: 1,
+		  blocks: 1,
+		  slam_dunks: 0
+		},
+		{
+		  player_name: "Kemba Walker",
+		  number: 33,
+		  shoe: 15,
+		  points: 6,
+		  rebounds: 12,
+		  assists: 12,
+		  steals: 7,
+		  blocks: 5,
+		  slam_dunks: 12
+		}
+	  ]
+	}
   }
 end
 
 # Write code here
+
+def all_players
+	game_hash[:home][:players] + game_hash[:away][:players]
+end
+
+def find_player(name)
+	all_players.find { |player| player[:player_name] == name }
+end
+
+# def find_team(name)
+# 	game_hash.map do |team, data|
+# 		if data[:team_name] == name
+# 			return data #need to check if there is a better solution
+# 		end
+# 	end
+# end
+
+
+def find_team(name)
+	r_team = game_hash.map do |team, data|
+		if data[:team_name] == name
+			data #need to check if there is a better solution
+		end
+	end
+    r_team.value
+end
+
+
+def num_points_scored(name)
+	find_player(name)[:points]
+end
+
+def shoe_size(name)
+	find_player(name)[:shoe]
+end
+
+def team_colors(name)
+	find_team(name)[:colors]
+end
+
+def team_names
+	[game_hash[:home][:team_name], game_hash[:away][:team_name]]
+end
+
+def player_numbers(name)
+	jersey_numbers = []
+	find_team(name)[:players].each do |player|
+		jersey_numbers.push(player[:number])
+	end
+	jersey_numbers
+end
+
+def player_stats(name)
+	find_player(name)
+end
+
+def big_shoe_rebounds
+	all_players.sort_by { |player| player[:shoe] }.last[:rebounds]
+end
